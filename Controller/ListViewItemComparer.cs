c7b9f04c87a6e1bf2f91ad3cf86aca22c0570343
LucasDos96 <dossantos.lucas12@gmail.com>
2020-06-10 00:28:24 +0200

    Ajout de ItemComparerclasse

---
+++ Controller/ListViewItemComparer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bacchus.Controller
+{
+    class ListViewItemComparer : IComparer
+    {
+        private int SortColumn;
+        private SortOrder order;
+
+        /// <summary>
+        /// Constructeur par defaut
+        /// </summary>
+        public ListViewItemComparer()
+        {
+            this.SortColumn = 0;
+            this.order = SortOrder.None;
+        }
+
+        /// <summary>
+        /// Constructeur par de recopie de la colonne
+        /// </summary>
+        public ListViewItemComparer(int ColumnToSort)
+        {
+            this.SortColumn = ColumnToSort;
+            this. order = SortOrder.Ascending;
+        }
+
+        public int Compare(object firstObject, object secondObject)
+        {
+            int res = 0;
+            var firstItem = (ListViewItem)firstObject;
+            var secondItem = (ListViewItem)secondObject;
+
+            switch (this.SortColumn)
+            {
+                case 3:
+                    // Si on trie par quantité
+                    int firstItemInt = Convert.ToInt32(firstItem.SubItems[0].Text);
+                    int secondItemInt = Convert.ToInt32(secondItem.SubItems[0].Text);
+
+                    res = firstItemInt - secondItemInt;
+                    break;
+                default:
+                    // Compare les deux chaines de caractères
+                    res = String.Compare(firstItem.SubItems[this.SortColumn].Text, secondItem.SubItems[this.SortColumn].Text);
+                    break;
+            }
+
+            return res;
+        }
+    }
+}
